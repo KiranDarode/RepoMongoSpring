3fa4b3f10c83103ea815bdadd4a6699994116748
kiran <kirandaode04@gmail.com>
2022-10-03 02:22:13 +0530

    first commit

--- src/main/java/com/petient/Cotroller/PatientController.java
+++ src/main/java/com/petient/Cotroller/PatientController.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.http.ResponseEntity;
+
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
